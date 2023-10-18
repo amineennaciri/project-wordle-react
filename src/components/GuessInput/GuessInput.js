3374eeec2c68b59136f613071e18d39cc521b901
amineennaciri <spitlight.enn@gmail.com>
2023-10-18 11:45:36 +0100

    exercise 2 done

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 function GuessInput() {
   const [guessInput, setGuessInput] = React.useState('');
+  const [guessTrials, setGuessTrials] = React.useState([]);
   function handleChange(event){
     if(guessInput.length>=5){
       return;
@@ -9,13 +10,19 @@ function GuessInput() {
     setGuessInput(event.target.value.toUpperCase());
   }
   return (
-    <>
+    <>  
+        <div className="guess-results">
+          {guessTrials.map(el=>(
+            <p className="guess" key={crypto.randomUUID()}>{el}</p>
+          ))}
+        </div>
         <form className="guess-input-wrapper" onSubmit={ (event) => {
           event.preventDefault();
           if(guessInput.length<5){
             return;
           }
           console.log({guess : guessInput});
+          setGuessTrials((prevState) => [...prevState, guessInput]);
           setGuessInput('');
         }}>
           <label htmlFor="guess-input">Enter guess:</label>
